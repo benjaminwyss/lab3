7eca9d77c2626457c50fbd52f9ceed5038b9e24d
BenjaminWyss <b040w377@ku.edu>
2019-02-21 13:31:19 -0600

    Implement picture switching functions.

--- slideshow.js
+++ slideshow.js
@@ -1,9 +1,30 @@
+let imageIndex = 0
+let imageMax = 5
+let images = ["cow.jpg", "pig.jpg", "chicken.jpg", "lamb.jpg", "horse.jpg"]
 
 function previous()
 {
-
+  if (imageIndex == 0)
+  {
+    imageIndex = imageMax - 1
+  }
+  else
+  {
+    imageIndex--
+  }
+  let picture = document.getElementById('image')
+  picture.src = images[imageIndex]
 }
 function next()
 {
-
+  if (imageIndex == (imageMax - 1))
+  {
+    imageIndex = 0
+  }
+  else
+  {
+    imageIndex++
+  }
+  let picture = document.getElementById('image')
+  picture.src = images[imageIndex]
 }
