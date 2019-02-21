16b529b9f1f4e2f2cd6cf35fdcfcb6c0d5877158
BenjaminWyss <b040w377@ku.edu>
2019-02-21 13:03:22 -0600

    Implement validate function.

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,4 +1,17 @@
 function validate()
 {
-  let pas = document.getElementById("password")
+  let password = document.getElementById("password")
+  let confirm = document.getElementById("confirm")
+  if (password.value != confirm.value)
+  {
+    alert("Passwords entered do not match!")
+  }
+  else if (password.value.length < 8)
+  {
+    alert("Passwords must be at least 8 characters in length.")
+  }
+  else
+  {
+    alert("Password successfully verified.")
+  }
 }
