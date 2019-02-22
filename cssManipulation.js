2006e8b42298583d1104874942bbaff87dd110a6
benjaminwyss <wyssbenjamin17@gmail.com>
2019-02-21 19:18:55 -0600

    Add javascript functionality to cssManipulator webpage.

--- cssManipulation.js
+++ cssManipulation.js
@@ -1,1 +1,15 @@
-function updateBorder()
+function update()
+{
+    let paragraph = document.getElementById("paragraph")
+    let borderRed = document.getElementById("borderRed")
+    let borderGreen = document.getElementById("borderGreen")
+    let borderBlue = document.getElementById("borderBlue")
+    let borderWidth = document.getElementById("borderWidth")
+    let backgroundRed = document.getElementById("backgroundRed")
+    let backgroundGreen = document.getElementById("backgroundGreen")
+    let backgroundBlue = document.getElementById("backgroundBlue")
+
+    paragraph.style.backgroundColor = "rgb(" + backgroundRed.value + ", " + backgroundGreen.value + ", " + backgroundBlue.value + ")"
+    paragraph.style.borderColor = "rgb(" + borderRed.value + ", " + borderGreen.value + ", " + borderBlue.value + ")"
+    paragraph.style.borderWidth = borderWidth.value + "px"
+}
